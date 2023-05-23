2bfd3ee0e227a8b08ac6fc3e9c28355489119eec
Felipe Sereno Soso <47258561+felipesereno@users.noreply.github.com>
2023-05-23 17:37:10 -0300

    Update apache_status.sh
    Retirada das mensagens ao usuário que não são armazenadas nos arquivos de log.

--- apache_status.sh
+++ apache_status.sh
@@ -6,21 +6,15 @@ offline_file="/home/ec2-user/nfs/offline.log"
 current_date=$(TZ=America/Sao_Paulo date +"%d-%m-%Y %H:%M:%S")
 
 if systemctl is-active --quiet httpd; then
-    echo "Serviço do Apache está ativo."
     if [ ! -f "$online_file" ]; then
-        echo "Criando arquivo $online_file..."
         echo "[$current_date] + APACHE + ONLINE + SERVIDOR WEB ATIVO." > "$online_file"
     else
-        echo "Adicionando mensagem de log a $online_file..."
         echo "[$current_date] + APACHE + ONLINE + SERVIDOR WEB ATIVO." >> "$online_file"
     fi
 else
-    echo "Serviço do Apache está inativo."
     if [ ! -f "$offline_file" ]; then
-        echo "Criando arquivo $offline_file..."
         echo "[$current_date] + APACHE + OFFLINE + SERVIDOR WEB INATIVO." > "$offline_file"
     else
-        echo "Adicionando mensagem de log a $offline_file..."
         echo "[$current_date] + APACHE + OFFLINE + SERVIDOR WEB INATIVO." >> "$offline_file"
     fi
 fi
