14e43afe218251ca1b7de35288eef13fcc2b7582
Felipe Sereno Soso <47258561+felipesereno@users.noreply.github.com>
2023-05-23 12:46:24 -0300

    Update apache_status.sh
    Adicionadas melhorias às mensagens ao usuário.

--- apache_status.sh
+++ apache_status.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-online_file="/home/ec2-user/nfs/online.txt"
-offline_file="/home/ec2-user/nfs/offline.txt"
+online_file="/home/ec2-user/nfs/online.log"
+offline_file="/home/ec2-user/nfs/offline.log"
 
 current_date=$(TZ=America/Sao_Paulo date +"%d-%m-%Y %H:%M:%S")
 
-# Check if Apache service is running
 if systemctl is-active --quiet httpd; then
-    echo "Apache server is running."
-    if [ ! -f "$online_file" ]; then
-        echo "Creating $online_file..."
+    echo "Serviço do Apache está ativo."
+    if [ ! -f "$online_file" ]; thenvim
+        echo "Criando arquivo $online_file..."
         echo "[$current_date] + APACHE + ONLINE + SERVIDOR WEB ATIVO." > "$online_file"
     else
-        echo "Appending message to $online_file..."
+        echo "Adicionando mensagem de log a $online_file..."
         echo "[$current_date] + APACHE + ONLINE + SERVIDOR WEB ATIVO." >> "$online_file"
     fi
 else
-    echo "Apache server is not running."
+    echo "Serviço do Apache está inativo."
     if [ ! -f "$offline_file" ]; then
-        echo "Creating $offline_file..."
+        echo "Criando arquivo $offline_file..."
         echo "[$current_date] + APACHE + OFFLINE + SERVIDOR WEB INATIVO." > "$offline_file"
     else
-        echo "Appending message to $offline_file..."
+        echo "Adicionando mensagem de log a $offline_file..."
         echo "[$current_date] + APACHE + OFFLINE + SERVIDOR WEB INATIVO." >> "$offline_file"
     fi
 fi
